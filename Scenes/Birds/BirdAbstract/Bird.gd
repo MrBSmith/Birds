97c59b721db46a192a31c801adcac62cafd6e2ff
Hugo Baba <Hugobarette@gmail.com>
2023-06-27 16:22:34 +0200

    Sparrow

---
+++ Scenes/Birds/BirdAbstract/Bird.gd
@@ -0,0 +1,47 @@
+extends Node2D
+class_name Bird
+
+@export_range(0.0, 9999.0) var speed : float = 200.0
+
+var direction := Vector2.RIGHT:
+	set(value):
+		if value != direction:
+			direction = value
+			direction_changed.emit(direction)
+
+signal started_moving
+signal stopped_moving
+signal direction_changed(dir: Vector2)
+
+
+func move_to_spot(spot: BirdSpot) -> void:
+	spot.bird = self
+	
+	await fly(spot.global_position)
+	
+	started_moving.connect(spot.set.bind("bird", null), CONNECT_ONE_SHOT)
+
+
+func fly(to: Vector2) -> void:
+	started_moving.emit()
+	
+	var tween = create_tween()
+	var duration = global_position.distance_to(to) / speed if speed != 0.0 else 0.0
+	
+	direction = global_position.direction_to(to)
+	tween.tween_property(self, "position", to, duration)
+	
+	await tween.finished
+	stopped_moving.emit()
+
+
+func seek_spot() -> BirdSpot:
+	var spots_array = get_tree().get_nodes_in_group("BirdSpot") as Array[BirdSpot]
+	spots_array.shuffle()
+	
+	for spot in spots_array:
+		if spot.is_free():
+			return spot
+	
+	return null
+
--- Scenes/Birds/States/IdleState_Bird.gd
+++ Scenes/Birds/States/IdleState_Bird.gd
@@ -3,16 +3,6 @@ extends StateMachine
 
 var current_spot : BirdSpot = null
 
-const NO_MOVABLE_ERROR = "This node doesn't have any usable MoveComponent, it can't perform any movement"
-
-@onready var default_move_component : MoveComponent = _fetch_default_move_component()
-
-@export var fly_component : MoveComponent
-@export var walk_component : MoveComponent
-@export var swim_component : MoveComponent
-
-@export var seek_spot_component : SeekLandSpotComponent
-
 @export var wait_timer : Timer
 
 @export_range(0.0, 99.0) var avg_wait_time : float
@@ -27,42 +17,11 @@ func _ready() -> void:
 func enter_state() -> void:
 	if wait_timer:
 		wait_timer.start(randfn(avg_wait_time, wait_time_variance))
-	
-	if !default_state:
-		return
-	
-	if current_spot:
-		if current_spot.type & BirdSpot.TYPE.WATER:
-			set_state("Water")
-		else:
-			set_state("Floor")
-
-
-func _fetch_default_move_component() -> MoveComponent:
-	for comp in [fly_component, walk_component, swim_component]:
-		if comp != null:
-			return comp
-	return null
 
 
 func _on_wait_timer_timeout() -> void:
-	if is_current_state() and seek_spot_component:
-		var spot = seek_spot_component.seek_spot()
-		var move_comp : MoveComponent
+	if is_current_state():
+		var spot = owner.seek_spot()
 		
-		if current_spot and spot.type == current_spot.type:
-			if spot.type & BirdSpot.TYPE.WATER and swim_component:
-				move_comp = swim_component
-			
-			elif spot.type & BirdSpot.TYPE.GROUND and walk_component:
-				move_comp = walk_component
-		
-		if move_comp == null:
-			if default_move_component:
-				move_comp = default_move_component
-			else:
-				push_error(NO_MOVABLE_ERROR)
-				return
-		
-		current_spot = spot
-		move_comp.move_to_spot(spot)
+		if spot:
+			owner.move_to_spot(spot)
